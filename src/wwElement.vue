08789f29ff225b136757d22744e51973f4faca9a
dorilama <assioma.xi@gmail.com>
2022-04-30 17:44:16 +0100

    create stripe instance

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,5 +1,42 @@
 <template></template>
 
-<script></script>
+<script>
+import { loadStripe } from "@stripe/stripe-js";
+
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  data() {
+    return { stripe: null };
+  },
+  computed: {
+    pubKey() {
+      // TODO figure out how to detect live app
+      return this.content.pubKeyTest;
+    },
+  },
+  watch: {
+    pubKey: {
+      async handler() {
+        if (!this.pubKey) {
+          return;
+        }
+        try {
+          this.stripe = await loadStripe(this.pubKey);
+        } catch (e) {
+          this.sendMessage("error", e);
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    sendMessage({ mode, obj }) {
+      // TODO send message back to app
+    },
+  },
+};
+</script>
 
 <style lang="css" scoped></style>
--- ww-config.js
+++ ww-config.js
@@ -5,5 +5,17 @@ export default {
     },
   },
   triggerEvents: [],
-  properties: {},
+  properties: {
+    pubKeyLive: {
+      label: { en: "LIVE publishable key" },
+      type: "Text",
+      section: "settings",
+      hidden: true, // TODO figure out how to detect live env and use this
+    },
+    pubKeyTest: {
+      label: { en: "TEST publishable key" },
+      type: "Text",
+      section: "settings",
+    },
+  },
 };
