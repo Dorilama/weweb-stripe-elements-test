632e6bf65337019aadaf1f5138330f92108615dc
dorilama <assioma.xi@gmail.com>
2022-04-30 17:11:09 +0100

    start from scratch

--- ww-config.js
+++ ww-config.js
@@ -4,55 +4,6 @@ export default {
       en: "Stripe Elements Test",
     },
   },
-  triggerEvents: [
-    {
-      name: "stripeLoaded",
-      label: { en: "On Stripe loaded" },
-      event: { value: "" },
-    },
-    {
-      name: "throwError",
-      label: { en: "On Error" },
-      event: { value: "" },
-    },
-  ],
-  properties: {
-    pubKey: {
-      label: {
-        en: "Publishable Key",
-      },
-      type: "Text",
-      defaultValue: "",
-    },
-    clientSecret: {
-      label: {
-        en: "PaymentIntent client secret",
-      },
-      type: "Text",
-      defaultValue: "",
-      bindable: true,
-    },
-    returnUrl: {
-      label: {
-        en: "Return Url",
-      },
-      type: "Text",
-      defaultValue: "",
-      bindable: true,
-    },
-    stripeLoaded: {
-      label: {
-        en: "stripe loaded",
-      },
-      type: "OnOff",
-      hidden: true,
-    },
-    error: {
-      label: {
-        en: "error",
-      },
-      type: "Object",
-      hidden: true,
-    },
-  },
+  triggerEvents: [],
+  properties: {},
 };
